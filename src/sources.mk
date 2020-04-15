ea0a7f5b86926260444142c5ede695edc8f4aeb8
Jeevaraam Kumar <jeevaraam@live.in>
2020-04-15 23:00:12 +0530

    Makefile Edits

--- src/sources.mk
+++ src/sources.mk
@@ -10,8 +10,7 @@
 #*****************************************************************************
 
 # Add your Source files to this variable
-SOURCES =
+SOURCES = main.c memory.c
 
 # Add your include paths to this variable
-INCLUDES = 
-
+INCLUDES = ../src/common/
